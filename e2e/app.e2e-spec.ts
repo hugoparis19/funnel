32078c6d2b4b96d602faf1828ac9a116aedb069d
Angular CLI <angular-cli@angular.io>
2017-06-29 17:23:24 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FunnelPage } from './app.po';
+
+describe('funnel App', () => {
+  let page: FunnelPage;
+
+  beforeEach(() => {
+    page = new FunnelPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('yuc works!');
+  });
+});
